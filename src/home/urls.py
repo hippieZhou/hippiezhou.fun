5309e6c2964763c2e8a58b85222d2dfa3400481d
hippieZhou <hippiezhou@outlook.com>
2019-09-02 22:59:02 +0800

    update

--- src/home/urls.py
+++ src/home/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'home'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home/index.html'), name='index'),
+    path('', views.index, name='index'),
     path('about/', TemplateView.as_view(template_name='home/about.html'), name='about'),
 ]
--- src/home/views.py
+++ src/home/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
+from .models import Visitor
 # Create your views here.
 
 
 def index(request):
+    def get_client_ip(request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
+
+    ip = get_client_ip(request)
+    print(ip)
     return render(request, 'home/index.html')
