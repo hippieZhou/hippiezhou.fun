c32286242ada032ae98abb70214e01c9ffc52ce7
hippieZhou <hippiezhou@outlook.com>
2019-09-07 11:02:14 +0800

    update

--- src/blog/views.py
+++ src/blog/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import render, get_object_or_404
 
 from django.views import generic
 
-from .models import Post
+from .models import ArticlePost
 # Create your views here.
 
 
 class IndexView(generic.ListView):
-    model = Post
+    model = ArticlePost
     template_name = 'blog/index.html'
     context_object_name = 'posts'
     paginate_by = 6
 
     def get_queryset(self):
-        return Post.objects.filter(published=True).order_by('-created')
+        return ArticlePost.objects.filter(published=True).order_by('-created')
 
 
 def detail(request, id, slug):
-    post = get_object_or_404(Post, id=id, slug=slug)
+    post = get_object_or_404(ArticlePost, id=id, slug=slug)
     return render(request, 'blog/detail.html', {'post': post})
